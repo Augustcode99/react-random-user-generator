e3e136094f6e310742f7e289ea36f68b1977d873
Augustcode99 <burakodabasi99@yahoo.com>
2023-06-09 16:20:28 +0300

    deleted unused code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { useState } from "react";
-import UseEffectHook from "./components/UseEffectHook";
 import User from "./components/User";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
-  const toggleEffect = () => {
-    setToggle(!toggle);
-  };
   return (
     <div>
-      {/* <button onClick={toggleEffect}>{toggle ? "hide" : "show"}</button>
-      {toggle && <UseEffectHook />} */}
       <User />
     </div>
   );
